e876539fbeec94e5344542209725224883447091
SimonSays2310 <129968845+SimonSays2310@users.noreply.github.com>
2024-10-24 00:27:20 +0700

    Fix shitcode

--- is.sh
+++ is.sh
@@ -15,16 +15,14 @@ is () {
 		fi
 	fi
 	if [[ $2 == "roughly_thirteen" ]]; then
-		result=$(printf "%.0f" $1)
-		if [[ $result -eq 13 ]]; then
+		if [[ $(printf "%.0f" $1) -eq 13 ]]; then
 			echo "true"
 		else
 			echo "false"
 		fi
 	fi
 	if [[ $2 == "yearofbirth" ]]; then
-		year=$(date +%Y)
-		result=$(echo "$year - $1" | bc)
+		result=$(echo "$(date +%Y) - $1" | bc)
 		if [[ $result -eq 13 ]]; then
 			echo "true"
 		else
@@ -32,32 +30,28 @@ is () {
 		fi
 	fi
 	if [[ $2 == "plus" ]] && [[ $4 == thirteen ]]; then
-		result=$(echo "$1 + $3" | bc)
-		if [[ $result -eq 13 ]]; then
+		if [[ $(echo "$1 + $3" | bc) -eq 13 ]]; then
 			echo "true"
 		else
 			echo "false"
 		fi
 	fi
 	if [[ $2 == "minus" ]] && [[ $4 == thirteen ]]; then
-		result=$(echo "$1 - $3" | bc)
-		if [[ $result -eq 13 ]]; then
+		if [[ $(echo "$1 - $3" | bc) -eq 13 ]]; then
 			echo "true"
 		else
 			echo "false"
 		fi
 	fi
 	if [[ $2 == "times" ]] && [[ $4 == thirteen ]]; then
-		result=$(echo "$1 * $3" | bc)
-		if [[ $result -eq 13 ]]; then
+		if [[ $(echo "$1 * $3" | bc) -eq 13 ]]; then
 			echo "true"
 		else
 			echo "false"
 		fi
 	fi
 	if [[ $2 == "dividedby" ]] && [[ $4 == "thirteen" ]]; then
-		result=$(echo "$1 / $3" | bc)
-		if [[ $result -eq 13 ]]; then
+		if [[ $(echo "$1 / $3" | bc) -eq 13 ]]; then
 			echo "true"
 		else
 			echo "false"
